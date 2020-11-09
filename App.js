2d63588768a03b76dcaa1ad8a528a2ad301bd812
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-08 22:13:40 -0600

    Se esta implementando funciones FireBase (1)

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
-  return(
+
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setUser(response);
+    });
+  }, []);
+
+  if (user === undefined) return null;
+
+  return (
     <SafeAreaView>
-      <Text>Fernando</Text>
+      {user ? <Text>Estas Logeado</Text> : <Text>No estas Logeado</Text>}
     </SafeAreaView>
   );
 }
