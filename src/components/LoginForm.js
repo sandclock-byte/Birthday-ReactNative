cd357a30abbd9b7fce76858fefba59d3ccda6026
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-12 12:43:33 -0600

    Se agrego estilo al login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+
+import { validateEmail } from '../utils/validations';
+import firebase from '../utils/firebase';
 
 export default function LoginForm(props) {
-    const {changeForm} = props;
+    const { changeForm } = props;
+
+    const login = () => {
+        console.log('Iniciando Sesión');
+    }
+
     return (
-        <View>
-            <Text>LoginForm</Text>
-            <TouchableOpacity onPress={changeForm}>
-                <Text style={styles.btnText}>Registrate</Text>
+        <>
+            <TextInput
+                style={styles.input}
+                placeholder='Correo electronico'
+                placeholderTextColor='#969696'
+            // onChange={(e) => setFormData({ ...formData, email: e.nativeEvent.text.trim() })}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Contraseña'
+                placeholderTextColor='#969696'
+                secureTextEntry={true}
+            // onChange={(e) => setFormData({ ...formData, email: e.nativeEvent.text.trim() })}
+            />
+            <TouchableOpacity onPress={login}>
+                <Text style={styles.btnText}>Iniciar sesión</Text>
             </TouchableOpacity>
-        </View>
+
+            <View style={styles.register}>
+                <TouchableOpacity onPress={changeForm}>
+                    <Text style={styles.btnText}>Registrate</Text>
+                </TouchableOpacity>
+            </View>
+
+        </>
     )
 }
 
@@ -17,5 +44,25 @@ const styles = StyleSheet.create({
     btnText: {
         color: '#FFF',
         fontSize: 18,
-   },
+    },
+
+    input: {
+        height: 50,
+        color: '#FFF',
+        width: '80%',
+        marginBottom: 25,
+        backgroundColor: '#1E3040',
+        paddingHorizontal: 20,
+        borderRadius: 50,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#1E3040',
+    },
+
+    register: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 10,
+    },
+
 })
