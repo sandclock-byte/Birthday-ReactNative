b00c7abb1d46ea8bf03e328a083f0e2a91ad0eb9
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-12 18:54:35 -0600

    Se está implementando estilo formulario

--- src/components/AddBirthday.js
+++ src/components/AddBirthday.js
@@ -1,21 +1,44 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 export default function AddBirthday() {
     return (
-        <View>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-            <Text>AddBirthday</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nombre'
+                    placeholderTextColor='#969696'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Apellidos'
+                    placeholderTextColor='#969696'
+                />
+            </View>
+        </>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    input: {
+        height: 50,
+        color: '#FFF',
+        width: '80%',
+        marginBottom: 25,
+        backgroundColor: '#1E3040',
+        paddingHorizontal: 20,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#1E3040',
+        borderRadius: 50,
+
+    }
+})
