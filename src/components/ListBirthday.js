d0afc68c75258b8e56e48f927dbbb96e93844a31
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-16 14:24:55 -0600

    Se estan ordenando la lista de cumpleaños

--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -4,6 +4,7 @@ import AddBirthday from './AddBirthday';
 import ActionBar from './ActionBar';
 import firebase from '../utils/firebase';
 import 'firebase/firestore';
+import moment from 'moment';
 
 firebase.firestore().settings({ experimentalForceLongPolling: true });
 const db = firebase.firestore(firebase);
@@ -12,24 +13,56 @@ export default function ListBirthday(props) {
     const { user } = props;
     const [showList, setShowList] = useState(true);
     const [birthday, setBirthday] = useState([]);
+    const [pasatBirthday, setPasatBirthday] = useState([]);
 
-    console.log(birthday);
 
     useEffect(() => {
-      setBirthday([]);
-      db.collection(user.uid)
-      .orderBy('dateBirth', 'asc')
-      .get()
-      .then((response) => {
-          const itemsArray = [];
-          response.forEach((doc) => {
-              const data = doc.data();
-              data.id = doc.id;
-              itemsArray.push(data);
-          });
-          setBirthday(itemsArray);
-      });  
-    }, [])
+        setBirthday([]);
+        setPasatBirthday([]);
+
+        db.collection(user.uid)
+            .orderBy('dateBirth', 'asc')
+            .get()
+            .then((response) => {
+                const itemsArray = [];
+                response.forEach((doc) => {
+                    const data = doc.data();
+                    data.id = doc.id;
+                    itemsArray.push(data);
+                });
+                formatData(itemsArray);
+            });
+    }, []);
+
+    const formatData = (items) => {
+        const currentDate = moment().set({
+            hour: 0,
+            minute: 0,
+            second: 0,
+            millisecond: 0,
+        });
+
+        const birthdayTempArray = [];
+        const pasatBirthdayTempArray = [];
+
+        items.forEach((item) => {
+            const dateBirth = new Date(item.dateBirth.seconds * 1000);
+            const dateBirthday = moment(dateBirth);
+            const currentYear = moment().get('year');
+            dateBirthday.set({ year: currentYear });
+
+            const diffDate = currentDate.diff(dateBirthday, 'days');
+            const itemTemp = item;
+            itemTemp.dateBirth = dateBirthday;
+            itemTemp.days = diffDate;
+
+            (diffDate <= 0) ? birthdayTempArray.push(itemTemp) : pasatBirthdayTempArray.push(itemTemp);
+
+            setBirthday(birthdayTempArray);
+            setPasatBirthday(pasatBirthdayTempArray);
+        });
+
+    }
 
     return (
         <View style={styles.container}>
