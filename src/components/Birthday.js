ca1d34c48564f265082ed066d8d1a77a8bff7b6d
Fernando Bernal Díaz <fernando.bernal@ditems.com>
2020-11-16 15:53:19 -0600

    Se está estilizando lista

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default function Birthday(props) {
     const { birthday } = props;
+    const pasat = birthday.days > 0 ? true : false;
     return (
-        <View>
+        <TouchableOpacity style= {[styles.card, pasat ? styles.pasat : birthday.days === 0 ? styles.actual : styles.current]}>
             <Text>
                 {birthday.name} {birthday.lastname}
             </Text>
-        </View>
+        </TouchableOpacity>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    card: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 60,
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        margin: 10,
+        borderRadius: 15,
+    },
+
+    actual: {
+        backgroundColor: '#559204',
+    },
+    
+    current: {
+        backgroundColor: '#1AE1F2',
+    },
+
+    pasat: {
+        backgroundColor: '#820000',
+    }, 
+
+})
